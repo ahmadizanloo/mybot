1935456adb2336e21c29a55910407340f35ac142
ahmadizanloo <119220889+ahmadizanloo@users.noreply.github.com>
2025-01-12 22:18:47 +0100

    main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def send_message():
         try:
             response = openai_client.chat.completions.create(
                 messages=st.session_state.user_history,
-                model="gpt-4o-audio-preview-2024-12-17",
+                model="gpt-3.5-turbo",
             )
             assistant_message = response.choices[0].message.content
 
